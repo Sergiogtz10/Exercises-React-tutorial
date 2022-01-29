9d016e9ec539904cf2990e9da376363974eb1248
Sergiogtz10 <sergiogutierrezcn@gmail.com>
2022-01-29 12:54:09 +0000

    feat:exercises complete

--- .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
+++ .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
@@ -7,11 +7,10 @@ const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
  * 1) Create the mapping function and use it to generate a new array of
  * planets in html called planetsInHTML
  */
-const mappingfunction = planets.map((planet, i) => {
-	return <li className="list-group-item">{planet}</li>;
-});
+
+
 
 // 2) add the array planetsInHTML inside the innerHTML of this ul
-const content = <ul className="list-group m-5">{mappingfunction}</ul>;
+const content = (<ul className="list-group m-5"></ul>);
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
--- .learn/resets/03.5-alert-component/app.jsx
+++ .learn/resets/03.5-alert-component/app.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
-const Alert = (props) => {
-	return (
-		<div className="alert alert-danger" role="alert">
-			{props.text}
-		</div>
-	);
-};
-Alert.PropTypes = {
-	text: PropTypes.string,
-};
+
+/**
+ * here you have declare your Alert component and return
+ * the html that bootstrap dictates for its alert component
+ */
+
 
 // here is where the alert component is being used, you don't have to edit this part,
 // but it helps you understan what properties is the component using
